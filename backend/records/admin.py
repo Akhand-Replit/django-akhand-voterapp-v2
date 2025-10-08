654e81fd29d78a7d052fc07c9a2fdfd2f5d82479
Akhand-Replit <replit@akhandfoundation.com>
2025-10-08 11:36:43 +0600

    Delete backend directory

--- backend/records/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Batch, Record, Event
-
-admin.site.register(Batch)
-admin.site.register(Record)
-# --- NEW: REGISTER EVENT MODEL ---
-admin.site.register(Event)
